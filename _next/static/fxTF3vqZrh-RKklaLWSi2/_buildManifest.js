b3237feb9849d70b6e3dac56e805329d2c28a5a7
Pattie Reaves <pattie@alley.co>
2020-11-22 22:15:42 -0700

    Updates

---
+++ _next/static/fxTF3vqZrh-RKklaLWSi2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-234e7e52c7606b7a2287.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cec57107aeb4d630844c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

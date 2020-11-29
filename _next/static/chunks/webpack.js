1b068735e56b44f50a02f8c1ca7d242f12cbf2ad
Pattie Reaves <pattie@alley.co>
2020-11-29 12:07:24 -0700

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6a11a17275c1c3556a3a";
+/******/ 	var hotCurrentHash = "341fc8d33399843ad9b5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/jo4EZRDizayqG6BqYxWvU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F1ca431d07698d53919cb.css","static\u002Fchunks\u002Fpages\u002Findex-62ba50ab0352b77104fa.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-720fd139030f349d0ede.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

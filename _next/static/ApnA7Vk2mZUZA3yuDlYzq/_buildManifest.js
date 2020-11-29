da2776273d98e0a9f1046ef6377dbfbd8f5b59d3
Pattie Reaves <pattie@alley.co>
2020-11-29 11:58:43 -0700

    Updates

---
+++ _next/static/ApnA7Vk2mZUZA3yuDlYzq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F023f83109fd5aa61e3a8.css","static\u002Fchunks\u002Fpages\u002Findex-653731d652631becaf40.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-720fd139030f349d0ede.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ae72569946f896fcacbf";
+/******/ 	var hotCurrentHash = "6a11a17275c1c3556a3a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
